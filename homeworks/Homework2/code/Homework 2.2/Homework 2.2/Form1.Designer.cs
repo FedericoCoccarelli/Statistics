a74f318a2b52b2894b64739382fdf3c31a6338ab
FedericoCoccarelli <Federicococcarelli@gmail.com>
2022-10-19 10:41:29 +0200

    Homework 2 app 2

--- homeworks/Homework2/code/Homework 2.2/Homework 2.2/Form1.Designer.cs
+++ homeworks/Homework2/code/Homework 2.2/Homework 2.2/Form1.Designer.cs
@@ -29,43 +29,101 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(338, 225);
+            this.button1.Location = new System.Drawing.Point(353, 13);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Parse";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 90);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(734, 330);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(444, 16);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(64, 19);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Header";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(272, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 24);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Upload file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(33, 92);
+            this.richTextBox1.Location = new System.Drawing.Point(22, 51);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(734, 96);
-            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Size = new System.Drawing.Size(734, 23);
+            this.richTextBox1.TabIndex = 5;
             this.richTextBox1.Text = "";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(525, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = " ";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private Button button1;
+        private DataGridView dataGridView1;
+        private CheckBox checkBox1;
+        private Button button2;
+        private OpenFileDialog openFileDialog1;
         private RichTextBox richTextBox1;
+        private ComboBox comboBox1;
     }
 }
--- homeworks/Homework2/code/Homework 2.2/Homework 2.2/Form1.cs
+++ homeworks/Homework2/code/Homework 2.2/Homework 2.2/Form1.cs
@@ -1,4 +1,7 @@
+using Microsoft.VisualBasic;
 using Microsoft.VisualBasic.FileIO;
+using System;
+
 namespace Homework_2._2
 {
     public partial class Form1 : Form
@@ -8,25 +11,52 @@ namespace Homework_2._2
             InitializeComponent();
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                richTextBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            comboBox1.Items.Clear();
+            bool hasheader = false;
+            string path = openFileDialog1.FileName;
+            var strings = File.ReadAllLines(path);
+            string[] row = strings[0].Split(",");
 
-            var path = @"C:\Users\Corsair\OneDrive\Cybersecurity\Statistics\Statistics Homeworks\homeworks\Homework2\code\Homework 2.2\trees.csv";
-            using (TextFieldParser parser = new TextFieldParser(@"C:\Users\Corsair\OneDrive\Cybersecurity\Statistics\Statistics Homeworks\homeworks\Homework2\code\Homework 2.2\trees.csv"))
+
+            if (!checkBox1.Checked)
             {
-                parser.TextFieldType = FieldType.Delimited;
-                parser.SetDelimiters(",");
-                    //Process row
-                var strings = File.ReadAllLines(path);
-                foreach (var s in strings)
+                for (int i = 0; i < row.Length; i++)
                 {
-                    var fields = s.Split(",");
-
-                    foreach (var field in fields)
-                        richTextBox1.Text = richTextBox1.Text + field;
+                    dataGridView1.Columns.Add("Field" + (i + 1).ToString(), "Field" + (i + 1).ToString());
+                    comboBox1.Items.Add("Field" + (i + 1).ToString());
                 }
+                hasheader = true;
             }
+
+            int length = strings.Length;
+            foreach (var s in strings)
+            {
+                var fields = s.Split(",");
+
+                if (!hasheader)
+                {
+                    foreach (var field in fields)
+                    {
+                        dataGridView1.Columns.Add(field, field);
+                        comboBox1.Items.Add(field);
+                    }
+                    hasheader = true;
+                }
+                else
+                    dataGridView1.Rows.Add(fields);
+            }
+            this.dataGridView1.AllowUserToAddRows = false;
         }
     }
 }
